1294459174f4f07d03af8fabf5c5c33f30ef498e
Ohki-ki <56137831+Ohki-ki@users.noreply.github.com>
2021-06-30 16:25:24 +0900

    Update HackRFID.ino

--- HackRFID.ino
+++ HackRFID.ino
@@ -6,62 +6,47 @@
 
  MFRC522 mfrc522(SS_PIN, RST_PIN);  // 创建MFRC522模型实例，定义ss和reset输入
 
+ MFRC522::MIFARE_Key key;
+ for (byte i = 0; i < 6; i++) key.keyByte[i] = 0x00; //初始化字典
+
+ int Key_1=0;
+ int Key_2=0;
+ int Key_3=0;
+ int Key_4=0;
+ int Key_5=0;
+ int Key_6=0;        
+
  void setup() 
  {
    SPI.begin(); // SPI初始化
    mfrc522.PCD_Init(); // MFRC522初始化
-  
+
  }
  
  void loop() 
  {
+  if ( ! MFRC522.PICC_IsNewCardPresent()) //检查是否有卡
+    return;
+ 
+  if ( ! MFRC522.PICC_ReadCardSerial()) // 验证NUID是否可读
+    return;
+ 
+  Serial.print("Card UID:");    //可读则输出UID
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  } 
+  Serial.print(" PICC type: ");   // 可读则输出卡的type
+  byte piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
+    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    Serial.println("不支持读取此卡类型");
+    return;
+  }else{
+  Serial.println(mfrc522.PICC_GetTypeName(piccType));
+  }
    
-   //Wait until new tag is available
-   while (getID()) 
-   {
-     lcd.clear();
-     lcd.setCursor(0, 0);
-     
-     if (tagID == MasterTag) 
-     {
-       
-       lcd.print(" Access Granted!");
-       // You can write any code here like opening doors, switching on a relay, lighting up an LED, or anything else you can think of.
-     }
-     else
-     {
-       lcd.print(" Access Denied!");
-     }
-     
-       lcd.setCursor(0, 1);
-       lcd.print(" ID : ");
-       lcd.print(tagID);
-       
-     delay(2000);
- ​
-     lcd.clear();
-     lcd.print(" Access Control ");
-     lcd.setCursor(0, 1);
-     lcd.print("Scan Your Card>>");
-   }
- }
- ​
- //Read new tag if available
- boolean getID() 
- {
-   // Getting ready for Reading PICCs
-   if ( ! mfrc522.PICC_IsNewCardPresent()) { //If a new PICC placed to RFID reader continue
-   return false;
-   }
-   if ( ! mfrc522.PICC_ReadCardSerial()) { //Since a PICC placed get Serial and continue
-   return false;
-   }
-   tagID = "";
-   for ( uint8_t i = 0; i < 4; i++) { // The MIFARE PICCs that we use have 4 byte UID
-   //readCard[i] = mfrc522.uid.uidByte[i];
-   tagID.concat(String(mfrc522.uid.uidByte[i], HEX)); // Adds the 4 bytes in a single String variable
-   }
-   tagID.toUpperCase();
-   mfrc522.PICC_HaltA(); // Stop reading
-   return true;
+  PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, (byte)1, &key, &uid);
+  
  }
